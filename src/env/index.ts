d72d5eb25e4574ba22aa20859e5bfe9520dd1f06
Vanessa Kaneko <vanessa.mitiek@gmail.com>
2024-09-26 16:35:34 -0300

    production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,8 +9,9 @@ if(process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(), // verifica se o valor dessa variável é uma string
-  PORT: z.number().default(3333) // se não tiver essa variável com um valor declarado, usa o 3333 por padrão
+  PORT: z.coerce.number().default(3333) // se não tiver essa variável com um valor declarado, usa o 3333 por padrão
 })
 
 const _env = envSchema.safeParse(process.env) /* safeParse faz as validações dos valores de acordo c/ os das variáveis no .env e
